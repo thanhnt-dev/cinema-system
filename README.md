b47841a95e50289d96993049caf51a5425514208
Nguyễn Tân Thành <92871790+NguyenTanThanh1403@users.noreply.github.com>
2024-04-16 18:35:04 +0700

    init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# cinema-system
+# cinema-system
---
+++ src/test/java/com/thanhnt/cinemasystem/CinemaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhnt.cinemasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
